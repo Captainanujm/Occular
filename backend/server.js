211e48d0730f2c75ec51b486638dfa8f11d7dd8a
Anuj mishra <captainanuj2004@gmail.com>
2025-11-09 20:03:21 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ await connectDB();
 
 const app = express();
 app.use(cors({
-  origin: ["https://occular-eqdo.vercel.app/"],
+  origin: "https://occular-eqdo.vercel.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));

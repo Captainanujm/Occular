9d2a663f954e4f550aec88d5e0da11f4a4fb62b5
Anuj mishra <captainanuj2004@gmail.com>
2025-11-10 11:36:06 +0530

    fix

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -22,7 +22,7 @@ export const sendContactMail = async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: true,
+      secure: false,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
